c07ae29c327addd541c5399ca9854c04f9c180d9
andro2121 <111317398+andro2121@users.noreply.github.com>
2022-09-26 19:38:14 +0200

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+adj = input("Please enter an adjective: ")
+verb1 = input ( "Please enter a verb: ")
+verb2 = input("Please enter another verb: ")
+famous_person = input("Please enter a famous person: ")
+madlib = f" \n Computer programming is so {adj}! it makes me excited all the time because I love \
+    to {verb1}.Stay hydrated and {verb2} like you are { famous_person}!"
+print(madlib)     
